c73e094d6fe65a99627bb96b16edf19a760c22b5
Jose Valim <joseaugustovalim1994@gmail.com>
2023-02-27 13:19:25 -0300

    Add: Adicionado e exemplos desafio 1 da seção 1

---
+++ Seção 1/Exemplos.js
@@ -0,0 +1,28 @@
+/* 
+Seção 1
+
+variavel var
+decimal
+undefinied
+string
+null
+boolean
+{}obejct 
+[]array
+
+> maior
+< menor
+>= maior
+<= menor
+
+== igual a
+!= diferente de
+=== igual e do mesmo tipo
+!== diferente mas do mesmo tipo
+
+Funções () parametros argumentos retorno
+function nomefuncao(parametro){
+  return (pode ter ou nao)
+}
+
+*/
