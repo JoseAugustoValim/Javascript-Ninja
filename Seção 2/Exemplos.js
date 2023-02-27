813e29486357ef55d058315e7454630ebe0deb4e
Jose Valim <joseaugustovalim1994@gmail.com>
2023-02-27 18:49:58 -0300

    Add: Adicionado  exemplos e desafios da seção 2

---
+++ Seção 2/Exemplos.js
@@ -0,0 +1,27 @@
+/*
+Operador Lógico
+&& - and
+|| - or
+! - not
+
+Operadores Unários
++ converte para numero exemplo + '3' = 3
++ concatenar (string + numero = string)
+- converte para numero exemplo - '3' = -3
+-
+
+Estrutura Léxica - nível mais baixo da linguagem
+case sensitive
+literais -  nao mudam
+identificadores - nomeam
+_ ou $
+
+
+Instruções Condicionais
+if (condição){
+
+}else{
+  
+}
+
+*/
