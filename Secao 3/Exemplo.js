cdde5fca7b19417a9d35da2c71957cb31302f7ff
Jose Valim <joseaugustovalim1994@gmail.com>
2023-02-28 16:42:10 -0300

    Add: Adicionado exemplo e challenges da Seção 3

---
+++ Secao 3/Exemplo.js
@@ -0,0 +1,44 @@
+/* 
+Tipos primitivos
+
+Tipos objetos - Organiza
+{propriedade:propriedade}
+var objeto = {
+  propriedade = valor,
+  propriedade2 = valor2,
+}
+objeto.propriedade
+
+Exemplo
+var pessoa = {
+  nome: Jose,
+  idade: 29,
+  peso: 85,
+  altura: 1.85
+}
+invocar: pessoa.nome 
+
+função anonima - função sem nome
+var myvar = function () {
+  return 'variavel myvar';
+};
+invocar myvar();
+
+Adicionar popriedade objeto
+propriedade.novaproprieddade : valor;
+
+Metodo objeto
+nome: function(){
+  return propriedade
+}
+*/
+var pessoa = {
+  nome: Jose,
+  idade: 29,
+  peso: 85,
+  altura: 1.85,
+};
+
+var myvar = function () {
+  return 'variavel myvar';
+};
