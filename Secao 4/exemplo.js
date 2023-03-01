73ef8bd30bd63244c101f794eee1c69ac6a8e1a5
Jose Valim <joseaugustovalim1994@gmail.com>
2023-03-01 15:16:48 -0300

    Add: Adicionado exemplo e challenge da seção 4

---
+++ Secao 4/exemplo.js
@@ -0,0 +1,30 @@
+/*
+Truthy
+Numero
+Retorna true
+Todos os outros numeros true
+
+Falsy
+Numero 
+retorna false
+undefined
+null
+nan
+0
+-0
+"" ou '' - string vazia
+
+Descobrir representação booleana
+!!
+
+Condicional Ternário
+condicão ? true : false;
+ 1 === 2 ? true : false;
+ instruções pequenas
+
+ Escopo variaveis e funções - local onde declaro uma variavel
+ Escopo Global - fora função
+ Escopo Local - dentro função
+
+ 
+*/
