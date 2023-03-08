d7bf6c4290239cd911fcbb2511ed6624b85f303d
Jose Valim <joseaugustovalim1994@gmail.com>
2023-03-08 09:37:48 -0300

    feat: Add Secao 5

---
+++ Secao 5/exemplo.js
@@ -0,0 +1,43 @@
+/*
+Retorno de função com arrays
+function myfunction(){
+  return [1,2,3];
+}
+myfunction()[0]
+
+function myFunction(){
+  {
+    prop1: Jose,
+    prop2: Valim,
+    prop3: function(){
+      return null;
+    }
+  }
+}
+myFunction().prop1;]
+
+var values = myFunction();
+
+Parametros funções
+var array = [1,2,3];
+function myFunction(argumento){
+  return argumento;
+}
+myFunction(array);
+function myFunction2(argumento){
+  return argumento[1];
+}
+myFunction2(arr);
+
+var obj = {
+  propriedade1: 'Jose',
+  curso: 'Javascript Ninja',
+  jogo: 'Atomic Heart'
+}
+myFunction(obj);
+myfunction(obj).jogo;
+
+function myFunction(arg){
+  return arg.curso;
+}
+*/
