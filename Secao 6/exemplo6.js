8ec6b914ef07c26cb3833471a44b5160498b0851
Jose Valim <joseaugustovalim1994@gmail.com>
2023-03-13 16:07:12 -0300

    Adicionado Seçoes 6 e 7

---
+++ Secao 6/exemplo6.js
@@ -0,0 +1,27 @@
+/* 
+Operador virgula e operacional switch
+var a,b = 1,2;
+function myFunction(){
+  var x=0;
+  return(x+=1,x) quer dizer que o Js primeiro executa o que esta antes da virgula e faz o retorno do que está depois 
+}
+Estruura Switch
+function(x){
+  swtich(x){
+    case 1:
+    console.log('O x é igual a 1');
+    break;
+    case 2:
+    console.log('O x é igual a 1');
+    break;
+    deafault:
+    cconsole.log('O x nae é igual a 1 e nem 2');
+  }
+}
+Estrutura While
+while(counter < 10){
+  console.log(counter);
+  counter ++;
+}
+
+*/
---
+++ Secao 7/exemplo07.js
@@ -0,0 +1,33 @@
+/*
+Operador módulo
+3 / 3 = 1
+3 % 3 = 0 de resto
+5 % 2 = 1 de resto
+
+Array Lenght - iteração
+var = ['Daciuk', {bola:azul}, function()];
+arr.lenght;
+var qtd = arr.lenght;
+while(qtd < 5){
+  console.log(qtd -= 1);
+}
+}
+
+while(qtd > 0){
+  console.log(arr[--qtd]);
+  if(qtd ===3){
+    console.log(arr[qtd.bola]);
+  }
+}
+
+Metodo Push e Loop for - adicionar um item no fim do array
+var array=[1,2,3,4,5];
+array.length();
+array.push({carro:'BMW'})
+
+for
+for(var i=0; i<5;i++){
+
+}
+
+*/
